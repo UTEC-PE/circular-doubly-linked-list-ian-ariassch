5408349e088aee577f9c26c06bf6ad26ab17b01c
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2018-09-04 14:01:28 -0500

    Agregado iteradores y lista con funciones

--- list.h
+++ list.h
@@ -9,27 +9,126 @@ using namespace std;
 
 template <typename T>
 class List {
-    private:
-        Node<T>* start;
+private:
+    Node<T>* start;
+    int nodes;
+public:
 
-    public:
-        List();
+    List(){
+        start = NULL;
+        nodes = 0;
+    };
 
-        T front();
-        T back();
-        void push_front(T value);
-        void push_back(T value);
-        void pop_front();
-        void pop_back();
-        T get(int position);
-        void concat(List<T> &other);
-        bool empty();
-        int size();
-        void clear();
-        Iterator<T> begin();
-        Iterator<T> end();
+    T front(){
+        if(nodes == 0)throw "Error";
+        return start-> data;
+    };
+    T back(){
+        return start->prev->data;
+    };
+    void print(){
+        Node<T> *temp= start;
+        for(int contador = 0; contador < nodes; contador++){
+            cout<<temp->data <<" ";
+            temp = temp->next;
+        }
+        delete temp;
+    };
+    void push_front(T value){
+        Node<T> *temp=new Node<T>;
+        temp->data=value;
+        if (nodes == 0){
+            start = new Node<T>;
+            start->data = temp->data;
+            start->next = start;
+            start->prev = start;
+        }
+        else{
+            temp->next = start;
+            temp->prev=start->prev;
+            start->prev->next=temp;
+            start->prev = temp;
+            start= temp;}
+        nodes++;
+    };
+    void push_back(T value){
+        Node<T> *temp=new Node<T>;
+        temp->data = value;
+        if (nodes == 0){
+            start = temp;
+            start -> next = start;
+            start -> prev = start;
+        }
+        else{
+            temp->prev = start->prev;
+            temp->next = start;
 
-        ~List();
+            start->prev->next = temp;
+            start->prev = temp;}
+        nodes++;
+    };
+    void pop_front(){
+        auto *temp=start;
+        if(nodes == 0)throw "Error";
+        if(nodes == 1){
+
+            start = NULL;
+            delete temp;
+
+        }
+        start->prev->next=start->next;
+        start = start->next;
+        start->prev = temp->prev;
+        delete temp;
+        nodes--;
+
+
+    };
+    void pop_back(){
+        if (nodes == 0)throw "Error";
+        if (nodes == 1){
+            start = NULL;
+        }
+        else{
+            auto *temp=start->prev;
+            start->prev= temp->prev;
+            temp->prev->next=start;
+            delete temp;}
+        nodes--;
+    };
+    T get(int position){
+        auto *temp = start;
+        if (nodes == 0)throw "Error";
+        if (position >= nodes) throw "Error";
+        for(int contador = 0; contador<nodes; contador++){
+            if(contador == position){
+                return temp->data;
+            }
+            temp = temp->next;
+        }
+    };
+    bool empty(){
+        return nodes == 0;
+    }
+    int size(){
+        return nodes;
+    }
+    void clear(){
+        if(nodes == 0)cout<<"Lista ya está vacia";
+        auto *temp=start->next;
+        while(temp != start){
+            auto *borrar = temp;
+            temp=temp->next;
+            delete borrar;
+        }
+        start = NULL;
+        delete temp;
+        nodes = 0;
+    };
+    Iterator<T> begin(){return Iterator<T>(start);}
+    Iterator<T> end(){return Iterator<T>(start->prev);}
+
+    ~List(){};
 };
 
 #endif
